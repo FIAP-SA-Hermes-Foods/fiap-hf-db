72bc7269db67f98c047b274e784c918ddadf790e
Leonardo Ierich <71282694+leonrd0@users.noreply.github.com>
2024-03-15 17:21:01 -0300

    Update db.tf

--- db.tf
+++ db.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "create_instance" {
   identifier            = "{{NAME_DB}}"
   engine                = "postgres"
   engine_version        = "16.1"
-  instance_class        = "db.t4.micro"
+  instance_class        = "db.t3.micro"
   allocated_storage     = 20
 
   username              = "{{USERNAME_DB}}"
@@ -33,7 +33,7 @@ resource "aws_db_instance" "create_instance" {
 }
 
 resource "aws_security_group" "rds" {
-  name = "test5"
+  name = "test6"
   vpc_id = "{{VPC_ID}}"
 
   ingress {
