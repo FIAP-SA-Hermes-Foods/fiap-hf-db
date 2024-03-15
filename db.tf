70fc080ea2a8e79b49d9982429f4c4fcee80f513
Leonardo Ierich <71282694+leonrd0@users.noreply.github.com>
2024-03-15 17:18:25 -0300

    Update db.tf

--- db.tf
+++ db.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "create_instance" {
   identifier            = "{{NAME_DB}}"
   engine                = "postgres"
   engine_version        = "16.1"
-  instance_class        = "db.t3.micro"
+  instance_class        = "db.t4.micro"
   allocated_storage     = 20
 
   username              = "{{USERNAME_DB}}"
