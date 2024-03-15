764f674b5c2843d55f0fece9dfec32184d15518a
Leonardo Ierich <71282694+leonrd0@users.noreply.github.com>
2024-03-15 16:27:27 -0300

    change to .tf

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sed -i s:{{NAME_DB}}:$NAME_DB:g ./db.hcl;
+sed -i s:{{NAME_DB}}:$NAME_DB:g ./db.tf;
 
-sed -i s:{{USERNAME_DB}}:$USERNAME_DB:g ./db.hcl;
-sed -i s:{{PASSWORD_DB}}:$PASSWORD_DB:g ./db.hcl;
+sed -i s:{{USERNAME_DB}}:$USERNAME_DB:g ./db.tf;
+sed -i s:{{PASSWORD_DB}}:$PASSWORD_DB:g ./db.tf;
 
-sed -i s:{{NAME_GROUP}}:$NAME_GROUP:g ./db.hcl;
-sed -i s:{{VPC_ID}}:$VPC_ID:g ./db.hcl;
+sed -i s:{{NAME_GROUP}}:$NAME_GROUP:g ./db.tf;
+sed -i s:{{VPC_ID}}:$VPC_ID:g ./db.tf;
 
-sed -i s:{{DB_FROM_PORT}}:$DB_FROM_PORT:g ./db.hcl;
-sed -i s:{{DB_TO_PORT}}:$DB_TO_PORT:g ./db.hcl;
+sed -i s:{{DB_FROM_PORT}}:$DB_FROM_PORT:g ./db.tf;
+sed -i s:{{DB_TO_PORT}}:$DB_TO_PORT:g ./db.tf;
